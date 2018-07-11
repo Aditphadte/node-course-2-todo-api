fbb01c5ff210b7894471ab08ccbc757ce3694b75
Your Na <you@example.com>
2018-07-11 09:39:16 +0530

    Add tests for PATCH /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -7,10 +7,10 @@ const {User}=require ('./../server/models/user');
 // Todo.remove({}).then((result)=>{
 //   console.log(result);
 // });
-
-Todo.findOneAndRemove({_id:'5b447a9b14e39c5cae98945e'}).then((todo)=>{
-
-});
+//
+// Todo.findOneAndRemove({_id:'5b447a9b14e39c5cae98945e'}).then((todo)=>{
+//
+// });
 
 Todo.findByIdAndRemove('5b447a9b14e39c5cae98945e').then((todo)=>{
   console.log(todo);
